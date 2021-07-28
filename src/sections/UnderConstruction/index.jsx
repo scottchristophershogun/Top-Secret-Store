a340f8f3f1406f7e6c5628692e9afc66e0c5a45d
scottchristophershogun <87443945+scottchristophershogun@users.noreply.github.com>
2021-07-28 10:14:13 -0400

    Create index.jsx

---
+++ src/sections/UnderConstruction/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from 'Components/Heading'
+import Flex from 'Components/Flex'
+
+import './styles.css'
+
+const Component = () => {
+  return (
+    <Flex height="10rem" align="center" justify="center">
+      <Heading as='h1'>Under construction. Come back later!</Heading>
+    </Flex>
+  )
+}
+
+export default Component
